77e01496120718fcb4bb6812e06725a338458ecb
V-Roman-V <vor.roman.v@gmail.com>
2022-06-09 16:24:47 +0300

    Update publisher

--- robot_communication/src/robot_communication.cpp
+++ robot_communication/src/robot_communication.cpp
@@ -2,30 +2,34 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 #include <cassert>
+#include <thread>
+
 #define assertm(exp, msg) assert(((void)msg, exp))
 
 #include "robot_communication/connection_to_robot.hpp"
 
 //Sets the parameters using ros
 class Param{
-  ros::NodeHandle nh;
+  ros::NodeHandle *nh;
 
 private:
   void setStringParam(const std::string& name, std::string& param, const std::string& dflt){
-    if (!nh.param<std::string>(name, param, dflt))
+    if (!nh->param<std::string>(name, param, dflt))
       ROS_WARN("%s parameter is set by default: %s",name.c_str(), dflt.c_str());
     else
       ROS_INFO("%s parameter is set: %s",name.c_str(), param.c_str());
   }
   void setIntParam(const std::string& name, int& param, const int& dflt){
-    if (!nh.param(name, param, dflt))
+    if (!nh->param(name, param, dflt))
       ROS_WARN("%s parameter is set by default: %d",name.c_str(), dflt);
     else
       ROS_INFO("%s parameter is set: %d",name.c_str(), param);
   }
 
 public:
-  Param(ros::NodeHandle& nh){
+  Param(ros::NodeHandle* nh)
+    :nh(nh)
+  {
     setStringParam("GlobalFrame", GlobalFrame, "map");
     setStringParam("RobotFrame", RobotFrame, "robot");
     setStringParam("OdometryTopic", OdometryTopic, "robot/odometry");
@@ -33,12 +37,8 @@ public:
     setIntParam("ControlHZ", ControlHZ, 500);
     setIntParam("OdometryHZ", OdometryHZ, 100);
 
-    if(ControlHZ == 0)
-      ROS_WARN("ControlHZ = 0, robot control is disabled");
-    if(OdometryHZ == 0)
-      ROS_WARN("OdometryHZ = 0, odometry publishing is disabled");
-    
-    this->nh = nh;
+    assertm(ControlHZ != 0,"ControlHZ = 0, change the value in config file");
+    assertm(OdometryHZ != 0,"OdometryHZ = 0, change the value in config file");
   }
     
   std::string GlobalFrame;
@@ -51,22 +51,16 @@ public:
   int OdometryHZ;
 };
 
-// class for publshing odometry from robot
-class Publisher{
-public:
-  Publisher(ros::NodeHandle& nh, const std::string& topic, RobotConnection* robot)
-    :robot(robot)
-  {
-    ros::Publisher odom_pub  = nh.advertise<nav_msgs::Odometry>(topic, 10);
-  }
-  void publish(){
-    odom_pub.publish( robot->getOdometry(ros::Time::now() ));
-  }
+// function for publishing odometry with some frequency
+void publisherThread(int frequency, RobotConnection* robot)
+{
+    ros::Rate rate(frequency);
 
-private:
-  RobotConnection* robot;
-  ros::Publisher odom_pub;
-};
+    while (ros::ok()) {
+      odom_pub.publish( robot->getOdometry(ros::Time::now() ));
+      rate.sleep();
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -80,27 +74,22 @@ int main(int argc, char **argv)
   ROS_INFO("Communication start at HIGH-level.\n");
 
   // Create topics
-  Publisher odom_pub(nh, param.OdometryTopic, &robot);
+  ros::Publisher odom_pub  = nh.advertise<nav_msgs::Odometry>(param.OdometryTopic, 10);
   ros::Subscriber twist_sub = nh.subscribe(param.VelocityTopic, 10, &RobotConnection::setVelocity, &robot); // set callback to robot.setVelocity
 
 
   // Creating loop functions
   LoopFunc loop_udpSend("udp_send", 1./param.ControlHZ, 3, boost::bind(&RobotConnection::UDPSend, &robot));
   LoopFunc loop_udpRecv("udp_recv", 1./param.OdometryHZ, 3, boost::bind(&RobotConnection::UDPRecv, &robot));
-  LoopFunc loop_pubOdom("pub_odom", 1./param.OdometryHZ,   boost::bind(&Publisher::publish,       &odom_pub));
-  
-  if(param.ControlHZ != 0){
-    loop_udpSend.start();
-  }
-  if(param.OdometryHZ != 0){
-    loop_udpRecv.start();
-    loop_pubOdom.start();
-  }
+  std::thread odom_thread(publisherThread, param.OdometryHZ, &robot);
+  loop_udpSend.start();
+  loop_udpRecv.start();
   
   robot.start_moving();
 
   ros::spin();
-
+  
+  odom_thread.join();
   robot.stop_moving();
   return 0;
 }
